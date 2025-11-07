80d1b5a31cf2b5c33b13db43b8c57c13a044a572
ShamilaHumayun123 <shamilahumayun942@gmail.com>
2025-11-07 18:34:18 +0500

    Save local changes before pull

--- frontend/config.js
+++ frontend/config.js
@@ -1,4 +1,4 @@
 //const API_BASE_URL = 'http://192.168.137.184:5000'; 
-export const API_BASE_URL = 'http://192.168.1.8:5000';
+export const API_BASE_URL = 'http://172.21.249.44:5000';
 //export const API_BASE_URL = 'http://localhost:5000';
 export default API_BASE_URL;
